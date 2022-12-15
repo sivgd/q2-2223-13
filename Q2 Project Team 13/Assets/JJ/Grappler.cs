addd7dc81d75ec3aa8ae310b3742c9a3c80ee915
MUKILTEO\1713529 <1713529@mukilteo.wednet.edu>
2022-12-15 10:06:45 -0800

    Updated grapple

--- Q2 Project Team 13/Assets/JJ/Grappler.cs
+++ Q2 Project Team 13/Assets/JJ/Grappler.cs
@@ -7,24 +7,50 @@ public class Grappler : MonoBehaviour
     public Camera mainCamera;
     public LineRenderer _lineRenderer;
     public DistanceJoint2D _distanceJoint;
+    public bool isAttachable;
 
     // Start is called before the first frame update
     void Start()
     {
         _distanceJoint.enabled = false;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+                if (hit)
+                {
+                    //Debug.Log(hit.collider.gameObject.tag);
+                    if(hit.collider.gameObject.tag == "isAttachable")
+                    {
+                        //Debug.Log("OK to attach...");
+                        Vector2 mousePos = (Vector2)mainCamera.ScreenToWorldPoint(Input.mousePosition);
+                        _lineRenderer.SetPosition(0, mousePos);
+                        _lineRenderer.SetPosition(1, transform.position);
+                        _distanceJoint.connectedAnchor = mousePos;
+                        _distanceJoint.enabled = true;
+                        _lineRenderer.enabled = true;
+
+
+                    }
+
+                }
+            }
+        }
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            Vector2 mousePos = (Vector2)mainCamera.ScreenToWorldPoint(Input.mousePosition);
-            _lineRenderer.SetPosition(0, mousePos);
-            _lineRenderer.SetPosition(1, transform.position);
-            _distanceJoint.connectedAnchor = mousePos;
-            _distanceJoint.enabled = true;
-            _lineRenderer.enabled = true;
+            //Vector2 mousePos = (Vector2)mainCamera.ScreenToWorldPoint(Input.mousePosition);
+            //_lineRenderer.SetPosition(0, mousePos);
+            //_lineRenderer.SetPosition(1, transform.position);
+            //_distanceJoint.connectedAnchor = mousePos;
+            //_distanceJoint.enabled = true;
+            //_lineRenderer.enabled = true;
         }
         else if (Input.GetKeyUp(KeyCode.Mouse0))
         {
