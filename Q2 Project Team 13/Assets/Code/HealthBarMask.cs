34f4aad0fe154e1e9e9476295e5e35afc0ea2470
Mint_Muncher <1713357@mukilteo.wednet.edu>
2023-01-05 08:57:42 -0800

    Commit to save work during room switch

---
+++ Q2 Project Team 13/Assets/Code/HealthBarMask.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBarMask : MonoBehaviour
+{
+    
+    public void MoveMask(int currentVal, int maxVal)
+    {
+        float percentToMove = (float)currentVal / (float)maxVal;
+        transform.localPosition = new Vector3(percentToMove - 1, 0, 0);
+    }
+
+}
