bfeee04a0c0f4c9491ec35a4c2413a45e6e7e29f
MUKILTEO\1713331 <RGJackdawnington@gmail.com>
2023-01-09 10:23:06 -0800

    Worked more on aniamtion transitions

--- Q2 Project Team 13/Assets/Code/SideMovement.cs
+++ Q2 Project Team 13/Assets/Code/SideMovement.cs
@@ -2,22 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class SideMovement : MonoBehaviour
 {
     public float accel = 8;
     private Rigidbody2D rb2;
     private SpriteRenderer sr;
+    Animator a;
     // Start is called before the first frame update
     void Start()
     {
         rb2 = GetComponent<Rigidbody2D>();
         sr = GetComponent<SpriteRenderer>();
+        a = gameObject.GetComponent<Animator>();
     }
 
 
     private void FixedUpdate()
     {
+        float horizValue = Input.GetAxis("Horizontal");
 
+        if(horizValue == 0)
+        {
+
+            a.SetBool("Moving", false);
+
+        }
+        else
+        {
+
+            a.SetBool("Moving", true);
+
+        }
         //Move right
         if (Input.GetAxis("Horizontal") > 0)
         {
