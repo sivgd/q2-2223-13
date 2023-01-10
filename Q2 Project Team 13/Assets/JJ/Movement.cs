2a3ca47940abf4880b4201a6e1bfdc4afe1dc544
MUKILTEO\1713529 <1713529@mukilteo.wednet.edu>
2023-01-10 10:05:33 -0800

    PLEASE WORK

--- Q2 Project Team 13/Assets/JJ/Movement.cs
+++ Q2 Project Team 13/Assets/JJ/Movement.cs
@@ -21,14 +21,17 @@ public class Movement : MonoBehaviour
         //Move right
         if (Input.GetAxis("Horizontal") > 0)
         {
-            sr.flipX = false;
+            //sr.flipX = false;
+            this.transform.localScale = new Vector3(1, 1, 1);
+            
             rb2.AddForce(new Vector2(accel, 0));
         }
 
         //Move left
         if (Input.GetAxis("Horizontal") < 0)
         {
-            sr.flipX = true;
+            //sr.flipX = true;
+            this.transform.localScale = new Vector3(-1, 1, 1);
             rb2.AddForce(new Vector2(-accel, 0));
         }
 
