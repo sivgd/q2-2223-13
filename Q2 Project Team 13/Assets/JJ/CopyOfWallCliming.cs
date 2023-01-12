c0041d6f9a960ea5154287f0c26e826408ed07ae
MUKILTEO\1713529 <1713529@mukilteo.wednet.edu>
2023-01-12 10:15:29 -0800

    lp

---
+++ Q2 Project Team 13/Assets/JJ/CopyOfWallCliming.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CopyOfWallCliming : MonoBehaviour
+{
+    public float accel = 8;
+    private Rigidbody2D rb2;
+    private SpriteRenderer sr;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2 = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        //Debug.Log(collision.tag);
+        if (collision.tag == "Wall")
+        {
+            Debug.Log("In wall...");
+
+            if (Input.GetKey(KeyCode.W))
+            {
+                //Debug.Log("WWWWWWWWWWWWWWWWWWWWWWWWW");
+                if (Input.GetAxis("Vertical") > 0)
+                {
+                    sr.flipY = false;
+                    rb2.AddForce(new Vector2(0, accel));
+                }
+                if (Input.GetAxis("Vertical") < 0)
+                {
+                    sr.flipY = true;
+                    rb2.AddForce(new Vector2(0, -accel));
+                }
+            }
+
+        }
+    }
+
+
+    private void Update()
+    {
+
+    }
+}
