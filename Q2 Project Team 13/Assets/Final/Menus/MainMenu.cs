a3b075c2749d27610394de3b5389fdb4ffa1d00b
MUKILTEO\1714014 <1714014@mukilteo.wednet.edu>
2023-01-23 09:23:17 -0800

    tutorial

--- Q2 Project Team 13/Assets/Final/Menus/MainMenu.cs
+++ Q2 Project Team 13/Assets/Final/Menus/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
 {
     public void StartGame()
     {
-        SceneManager.LoadScene("L1");
+        SceneManager.LoadScene("Xavierscene");
     }
     public void ShowCredits()
     {
---
+++ Q2 Project Team 13/Assets/Xavier/GameL1.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameL1 : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Debug.Log("Switch scene");
+            SceneManager.LoadScene("L1");
+        }
+    }
+
+}
